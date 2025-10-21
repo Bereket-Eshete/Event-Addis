367f90723b64dac6ee7395deda4a0d3e8a19b92e
Bereket-Eshete <bereketeshete63@gmail.com>
2025-10-21 08:37:03 +0300

    fix: add localhost url to the backend cors origin to allow access for dev enviroment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  //add the deve url to allow request from the localhost
   app.enableCors({
     origin: [process.env.FRONTEND_URL, 'http://localhost:3000'].filter(Boolean),
     credentials: true,
